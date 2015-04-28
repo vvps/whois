8cf6adec4b97bf3427897c3924f29ed0573fc2eb
Vijay <vijayparsi@gmail.com>
2015-04-28 17:33:32 +0200

    Update whois.py
    Works with python3 and with popen

--- whois.py
+++ whois.py
@@ -1,30 +1,45 @@
+#!/usr/bin/python3
+#coding: utf8
+
 import os
 import sys
 import fnmatch
-from PIL import Image
+import subprocess
 
-dirPath = r'I:\A_folder_with_photos_and_subfolders'
-
-if len(sys.argv) > 1:
-    searchParam = sys.argv[1]
-    regExpParam = '*' + searchParam + '*.*'
-else:
-    print('Enter a search parameter')
-    exit
-    
-if os.path.exists(dirPath):
-    os.chdir(dirPath)
-else:
-    print('Directory does not exist - Connect to server')
-
+dirPath = r'/cygdrive/folder/with/images'
+irfanview = r'/cygdrive/c/Program Files/IrfanView/i_view32.exe' # Choose your program
 
 def showImage(rootName, fileName):
-    print (os.path.join(rootName,fileName))
-    os.startfile(os.path.join(rootName,fileName))
-    return
+	#print ("\n %s" % os.path.join(rootName,fileName))
+	os.chdir(os.path.join(dirPath,rootName))
+	subprocess.Popen([irfanview,fileName])
+	return
 
+def main():
 
-for root, subDir, files in os.walk('.'):
-    for file in files:
-        if fnmatch.fnmatch(file, regExpParam):
-            showImage(root, file)
+	imgCounter = 0
+
+	if len(sys.argv) > 1:
+		searchParam = sys.argv[1]
+		regExpParam = '*' + searchParam + '*.*'
+	else:
+		print ("Enter a search parameter")
+		exit()
+	
+	if not os.path.exists(dirPath):
+		print ("\n Can\'t find directory %s - Connect to server" % dirPath)
+		exit()
+
+	for root, subDir, files in os.walk(dirPath):
+		for file in files:
+			if fnmatch.fnmatch(file, regExpParam):
+				showImage(root, file)
+				imgCounter += 1
+				
+	if (imgCounter == 0):
+		print("%s not found!" % searchParam)
+	else:
+		print("%d matches for %s" % (imgCounter,searchParam))
+
+if __name__ == '__main__': 
+    main()
